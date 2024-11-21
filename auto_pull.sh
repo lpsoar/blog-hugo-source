f63960dfe1782ba118335bd32517e4d25c1e3ebc
blazarix <1832912300@qq.com>
2024-11-22 00:47:34 +0800

    init

--- auto_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /www/wwwroot/hugo
-git pull
-echo "拉取最新文章成功,发布完成"
-
--- one.sh
+++
@@ -1,8 +0,0 @@
-git pull --rebase
-echo "拉取最新代码"
-git add . 
-echo "added"
-git commit -m "发布文章" 
-echo "commited"
-git push
-echo "推送到github成功"
